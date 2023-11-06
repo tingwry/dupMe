13b18edc8da404c4cb06f979f032366b9aa99926
Fahnalan <nalanlakwong@gmail.com>
2023-11-06 23:19:28 +0700

    successfully random avator

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,17 +4,16 @@ import "./App.css";
 
 import Room from "./pages/Room/Room";
 import Home from "./pages/Home/Home";
-import Avatar from "./components/Avatar/Avatar";
 
 function App() {
   return (
     <div className="App">
       <div className="tab">DupMe</div>
-      <Avatar />
-      {/* <Routes>
-                <Route path='/' element={ <Home /> } />
-                <Route path='/room' element={ <Room /> } />
-            </Routes> */}
+      {/* <Avatar /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/room" element={<Room />} />
+      </Routes>
     </div>
   );
 }
--- client/src/components/SubmitUser/SubmitUser.tsx
+++ client/src/components/SubmitUser/SubmitUser.tsx
@@ -1,35 +1,22 @@
-import React, { ChangeEvent, useState } from "react";
-import socket from "../../socket";
-import Avatar1Image from "../../assets/pictures/avatar1.png";
+import React from "react";
+import { AvatarGenerator } from "random-avatar-generator";
+import NameInput from "./NameInput";
 
 function SubmitUser() {
-  const [name, setName] = useState<string>("");
+  const generator = new AvatarGenerator();
 
-  const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setName(event.target.value);
-  };
+  // Simply get a random avatar
+  const randomavatar = generator.generateRandomAvatar();
 
-  const handleSubmitName = () => {
-    socket.emit("submit_name", name);
-    socket.connect();
-  };
   return (
-    <> 
-      <p>
-        <img
-          style={{ width: "189px", height: "189px", flexWrap: "wrap" }}
-          src={Avatar1Image}
-          alt="avatar1"
-        />
-      </p>
-      <input
-        type="text"
-        value={name}
-        onChange={handleNameChange}
-        placeholder="Enter your name"
+    <>
+      <img
+        style={{ width: "189px", height: "189px", flexWrap: "wrap" }}
+        src={randomavatar}
+        alt="avatar1"
       />
       <p></p>
-      <button onClick={handleSubmitName}>Submit</button>
+      <NameInput avatar={randomavatar} />
     </>
   );
 }
