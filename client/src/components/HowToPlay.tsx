0750ecf84227403ec584fbf742ce35253a8831fb
Fahnalan <nalanlakwong@gmail.com>
2023-11-07 22:23:36 +0700

    Done with how to play

--- client/src/components/HowToPlay.tsx
+++ client/src/components/HowToPlay.tsx
@@ -5,32 +5,22 @@ const HowToPlay = () => {
   return (
     <div>
       <h1>How to Play DupMe</h1>
+      <p>
+        "DupMe is a two-player game where players take turns creating and
+        following piano patterns.
+      </p>
+      <p> Players earn points for correctly mimicking each pattern. </p>
 
       <h2>Rules:</h2>
-      <p>- Rule 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      <p>
-        - Rule 2: Vestibulum tincidunt tortor vel orci consectetur, vel eleifend
-        urna dapibus.
-      </p>
-      {/* Add more rules as needed */}
+      <p>- The first player has 10 seconds to create a pattern on piano.</p>
+      <p>- Next, the second player has 20 seconds to follow the pattern.</p>
+      <p>- Then, the second player will start the next turn.</p>
+      <p>- The game will end after 2 rounds.</p>
 
-      <h2>Tips:</h2>
-      <p>
-        - Tip 1: Integer lobortis purus ut dui convallis, at iaculis dolor
-        vulputate.
-      </p>
-      <p>
-        - Tip 2: Morbi vitae lacus vel felis volutpat volutpat nec nec urna.
-      </p>
-      {/* Add more tips as needed */}
-
-      <h2>Guidelines:</h2>
-      <p>- Guideline 1: Sed eu ligula non nisi euismod finibus ut nec neque.</p>
-      <p>
-        - Guideline 2: Quisque lacinia nulla nec elit euismod, at semper massa
-        tempor.
-      </p>
-      {/* Add more guidelines as needed */}
+      <h2>Scoring:</h2>
+      <p>- Scores are increased based on the continous right steps.</p>
+      <p>- The right step has to be the right button and the right order.</p>
+      <p>- The one with the higher score is the winner!</p>
       <p>
         <Link to="/">
           <button>Back to Home</button>
