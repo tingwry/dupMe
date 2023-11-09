1cdc3ab420ebcd8c4d7a876c9b0348cdb724bb9a
tingwry <weeraya972546@gmail.com>
2023-11-09 20:46:20 +0700

    fix bg colors + deco

---
+++ client/src/components/Decorations/Decorations.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Decorations.css";
+
+const Decorations = () => {
+  return (
+    <>
+      <img className="rainbow" src="/decorations/rainbow.svg" />
+      <img className="joy" src="/decorations/joy.svg" />
+      <img className="sparkle" src="/decorations/sparkle.svg" />
+      <img className="radio" src="/decorations/radio.svg" />
+    </>
+  );
+};
+
+export default Decorations;
