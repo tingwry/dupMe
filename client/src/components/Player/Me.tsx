af083133184f514694616b76490475be64ebaf45
Fahnalan <nalanlakwong@gmail.com>
2023-11-09 19:33:11 +0700

    emoji reaction complete

--- client/src/components/Player/Me.tsx
+++ client/src/components/Player/Me.tsx
@@ -1,49 +1,64 @@
-import React, { useEffect, useState } from 'react'
-import socket from '../../socket'
-import './Player.css'
+import React, { useEffect, useState } from "react";
+import socket from "../../socket";
+import "./Player.css";
+
+import smile from "../../assets/pictures/smile.png";
+import smirk from "../../assets/pictures/smirk.png";
+import blah from "../../assets/pictures/blah.png";
+import angry from "../../assets/pictures/angry.png";
+import clown from "../../assets/pictures/clown.png";
+import aunji from "../../assets/pictures/aunji.png";
+import blank from "../../assets/pictures/blank.png";
 
 function Me() {
-    const [name, setName] = useState<string>();
-    const [avatar, setAvatar] = useState<string>();
-    const [score, setScore] = useState(0);
-    const [myReaction, settMyReaction] = useState<string>("Your reaction will be shown here");
-    const allReaction = ["yay", "sad", "bye", "hi"];
+  const [name, setName] = useState<string>();
+  const [avatar, setAvatar] = useState<string>();
+  const [score, setScore] = useState(0);
+  const [myReaction, settMyReaction] = useState<string>(blank);
+  const allReaction = [smile, smirk, blah, angry, clown, aunji];
 
-    const handleReaction = (item: string) => {
-        socket.emit('send_reaction', { reaction: item });
-        settMyReaction(item);
-    }
+  const handleReaction = (item: string) => {
+    socket.emit("send_reaction", { reaction: item });
+    settMyReaction(item);
+  };
 
-    useEffect(() => {
-        socket.on('me', (data) => {
-            setName(data.name);
-            setAvatar(data.avatar);
-            setScore(data.score);
-        })
-    }, [socket])
+  useEffect(() => {
+    socket.on("me", (data) => {
+      setName(data.name);
+      setAvatar(data.avatar);
+      setScore(data.score);
+    });
+  }, [socket]);
 
-    return (<>
-        <h3>You</h3>
-        <img
-            // style={{ width: "100px", height: "100px", flexWrap: "wrap" }}
-            className='avatar'
-            src={avatar}
-            alt="Me"
-        />
-        <div className='name'>{name}</div>
-        <div>current score: {score}</div>
+  return (
+    <>
+      <h3>You</h3>
+      <img
+        // style={{ width: "100px", height: "100px", flexWrap: "wrap" }}
+        className="avatar"
+        src={avatar}
+        alt="Me"
+      />
+      <div className="name">{name}</div>
+      <div>current score: {score}</div>
 
-        <div className='reaction-bubble'>{myReaction}</div>
-        <h3>Your reaction</h3>
-        {allReaction.map((item) => (
-            <button 
-                key={item} 
-                onClick={() => {handleReaction(item)}}
-            >
-                {item}
-            </button>
-        ))}
-    </>)
+      <div className="reaction-bubble">
+        <img src={`${myReaction}`} style={{ width: 30, height: 30 }} />
+      </div>
+      <h3>Your reaction</h3>
+      {allReaction.map((item) => (
+        <button
+          key={item}
+          onClick={() => {
+            handleReaction(item);
+          }}
+          className="reaction-button"
+        >
+          <img src={`${item}`} style={{ width: 25, height: 25 }} />
+        </button>
+      ))}
+    </>
+  );
 }
 
-export default Me
+export default Me;
--- client/src/components/Player/Opponent.tsx
+++ client/src/components/Player/Opponent.tsx
@@ -1,37 +1,46 @@
-import React, { useEffect, useState } from 'react'
-import socket from '../../socket'
-import './Player.css'
+import React, { useEffect, useState } from "react";
+import socket from "../../socket";
+import "./Player.css";
+
+import smile from "../../assets/pictures/smile.png";
+import smirk from "../../assets/pictures/smirk.png";
+import blah from "../../assets/pictures/blah.png";
+import angry from "../../assets/pictures/angry.png";
+import clown from "../../assets/pictures/clown.png";
+import aunji from "../../assets/pictures/aunji.png";
+import blank from "../../assets/pictures/blank.png";
 
 function Opponent() {
-    const [name, setName] = useState<string>();
-    const [avatar, setAvatar] = useState<string>();
-    const [score, setScore] = useState(0);
+  const [name, setName] = useState<string>();
+  const [avatar, setAvatar] = useState<string>();
+  const [score, setScore] = useState(0);
 
-    const [oppoentReaction, settOpponenReaction] = useState<string>("Your opponent's reaction will be shown here");
+  const [oppoentReaction, settOpponenReaction] = useState<string>(blank);
 
-    useEffect(() => {
-        socket.on('opponent', (data) => {
-            setName(data.name);
-            setAvatar(data.avatar);
-            setScore(data.score);
-        })
+  useEffect(() => {
+    socket.on("opponent", (data) => {
+      setName(data.name);
+      setAvatar(data.avatar);
+      setScore(data.score);
+    });
 
-        socket.on('receive_reaction', (data) => {
-            settOpponenReaction(data.reaction);
-        })
-    }, [socket])
+    socket.on("receive_reaction", (data) => {
+      settOpponenReaction(data.reaction);
+      console.log(data.reaction);
+    });
+  }, [socket]);
 
-    return (<>
-        <h3>Opponent</h3>
-        <img
-            className='avatar'
-            src={avatar}
-            alt="Opponent"
-        />
-        <div className='name'>{name}</div>
-        <div>current score: {score}</div>
-        <div className='reaction-bubble'>{oppoentReaction}</div>
-    </>)
+  return (
+    <>
+      <h3>Opponent</h3>
+      <img className="avatar" src={avatar} alt="Opponent" />
+      <div className="name">{name}</div>
+      <div>current score: {score}</div>
+      <div className="reaction-bubble">
+        <img src={`${oppoentReaction}`} style={{ width: 25, height: 25 }} />
+      </div>
+    </>
+  );
 }
 
-export default Opponent
+export default Opponent;
